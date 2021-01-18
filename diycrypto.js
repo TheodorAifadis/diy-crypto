6843d3256dac595759cf15d10253c54ea20fea0b
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2021-01-18 16:02:19 +0100

    commit-3

---
+++ diycrypto.js
@@ -0,0 +1,36 @@
+export function caesarEncrypt(msg, shift) {
+
+    const charCodes = []
+    for (let i = 0; i < msg.length; i++) {
+        charCodes.push(msg[i].charCodeAt(0))
+    }
+
+    for (let i = 0; i < charCodes.length; i++) {
+        charCodes[i] = charCodes[i] + shift
+    }
+
+    let result = ''
+    for (let i = 0; i < charCodes.length; i++) {
+        result = result + String.fromCharCode(charCodes[i])
+    }
+
+    return result
+}
+
+export function caesarDecrypt(encryptedMsg, shift) {
+    const charCodes = []
+    for (let i = 0; i < encryptedMsg.length; i++) {
+        charCodes.push(encryptedMsg[i].charCodeAt(0))
+    }
+
+    for (let i = 0; i < charCodes.length; i++) {
+        charCodes[i] = charCodes[i] - shift
+    }
+
+    let result = ''
+    for (let i = 0; i < charCodes.length; i++) {
+        result = result + String.fromCharCode(charCodes[i])
+    }
+
+    return result
+}
--- main.js
+++ main.js
@@ -1,43 +1,5 @@
+import { caesarEncrypt, caesarDecrypt } from "./diycrypto.js"
 
-function caesarEncrypt(msg, shift) {
+console.log(caesarEncrypt('HELLO', 1))
+console.log(caesarDecrypt('HELLO', 0))
 
-    const charCodes = []
-    for (let i = 0; i < msg.length; i++) {
-        charCodes.push(msg[i].charCodeAt(0))
-    }
-
-    for (let i = 0; i < charCodes.length; i++) {
-        charCodes[i] = charCodes[i] + shift
-    }
-
-    let result = ''
-    for (let i = 0; i < charCodes.length; i++) {
-        result = result + String.fromCharCode(charCodes[i])
-    }
-
-    return result
-}
-
-function caesarDecrypt(encryptedMsg, shift) {
-    const charCodes = []
-    for (let i = 0; i < encryptedMsg.length; i++) {
-        charCodes.push(encryptedMsg[i].charCodeAt(0))
-    }
-
-    for (let i = 0; i < charCodes.length; i++) {
-        charCodes[i] = charCodes[i] - shift
-    }
-
-    let result = ''
-    for (let i = 0; i < charCodes.length; i++) {
-        result = result + String.fromCharCode(charCodes[i])
-    }
-
-    return result
-}
-
-const secretMessage = 'RETREATTOMMORROW'
-const encryptedMessage = caesarEncrypt(secretMessage, 5)
-const decryptedMessage = caesarDecrypt(encryptedMessage, 5)
-
-console.log(secretMessage, encryptedMessage, decryptedMessage)
